7b01f114d3d93761c38c714adfb32664f62bec26
Casey Kerr <masterprompt@gmail.com>
2024-12-20 09:40:57 -0600

    updates

---
+++ src/features/app/index.tsx
@@ -0,0 +1,11 @@
+import { PrimeReactProvider } from 'primereact/api';
+
+export const App = () => {
+    return (
+        <PrimeReactProvider>
+            <div>
+                <h1>Comming soon!</h1>
+            </div>
+        </PrimeReactProvider>
+    );
+}
